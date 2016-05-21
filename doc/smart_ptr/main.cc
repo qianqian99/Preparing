eda6a33ca4abc9cb5ed3538704477abcb7469621
hgg <hgg>
2016-05-21 18:15:20 +0800

    finish almost

--- doc/smart_ptr/main.cc
+++ doc/smart_ptr/main.cc
@@ -1,8 +1,19 @@
 #include "smart_ptr.hpp"
 using namespace std;
+struct A
+{
+    int a;
+    int b;
+    A() : a(10), b(20){}
+};
 int main()
 {
-    hgg::shared_ptr<int> sp(new int);
-    hgg::shared_ptr<int> spp(sp);
+    hgg::shared_ptr<A> sp1(new A);
+    hgg::shared_ptr<int> sp2(new int);
+    hgg::weak_ptr<A> wp1(sp1);
+    hgg::weak_ptr<int> wp2(sp2);
+    hgg::shared_ptr<int> sp3(sp2);
+    hgg::shared_ptr<int> sp4(wp2);
+    hgg::weak_ptr<A> wp3(wp1);
     return 0;
 }
--- doc/smart_ptr/smart_ptr.hpp
+++ doc/smart_ptr/smart_ptr.hpp
@@ -1,3 +1,4 @@
+#include <iostream>
 namespace hgg
 {
     class count_base
@@ -10,10 +11,21 @@ namespace hgg
             {
                 ++_use_count;
             }
+            void add_weak_ref()
+            {
+                ++_weak_count;
+            }
+            void reliease() {
+                if (--_use_count == 0) {
+                    delete this;
+                }
+            }
             count_base(): _use_count(1), _weak_count(1){}
             virtual ~count_base()=0;
     };
-    count_base::~count_base(){}
+    count_base::~count_base(){
+        std::cout << "~base" << std::endl;
+    }
     template <typename T>
     class count_dervied : public count_base
     {
@@ -21,10 +33,15 @@ namespace hgg
             T *_ptr;
         public:
             count_dervied(T *ptr) :_ptr(ptr) {}
+            ~count_dervied(){
+                std::cout << "~dervied" << std::endl;
+            }
     };
+    class weak_count;
     class shared_count
     {
         private:
+            friend class weak_count;
             count_base *_pi;
         public:
             shared_count() : _pi(0){}
@@ -40,16 +57,23 @@ namespace hgg
                 }
                 catch (...)
                 {
+                    std::cout << "can't get a new size" << std::endl;
                     return;
                 }
             }
+            shared_count(const weak_count &);
             void swap(shared_count &other)
             {
                 count_base *tmp = other._pi;
                 other._pi = _pi;
-                tmp = other._pi;
+                _pi = tmp;
+            }
+            ~shared_count() {
+                if (_pi != 0) _pi->reliease();
             }
     };
+    template <typename Type>
+    class weak_ptr;
     template <typename T>
         class shared_ptr
         {
@@ -57,6 +81,7 @@ namespace hgg
                 shared_count _pn;
                 T *_px;
             public:
+                template <typename Type> friend class weak_ptr;
                 typedef T value_type;
                 typedef shared_ptr<T> this_type;
                 /*依赖于定义的shared_count::swap()
@@ -66,21 +91,79 @@ namespace hgg
                     _px = other._px;
                     _pn.swap(other._pn);
                 }
+                T *get(){return _px;}
                 void construct_count(T *p, shared_count &pn)
                 {
+                    /*it will produce a new count_base obj*/
                     shared_count(p).swap(pn);
                 }
+                // p -> shared_ptr : construct_count() -> 
+                // shared_count()->new count_base: weak_count=use_count=1
                 shared_ptr(T *p) : _px(p), _pn(){
                     construct_count(p, _pn);
                 }
+                // shared_ptr -> shared_ptr: _pn.constructor-> copy
                 shared_ptr(const shared_ptr &other) 
                     : _px(other._px), _pn(other._pn)
                 {}
-                //shared_ptr &operator = (const shared_ptr &other)
-                //{
-                //    this_type().swap();
-                //}
+                // assignment : shared_ptr -> shared_ptr ->this_type().swap();
+                shared_ptr &operator = (const shared_ptr &other)
+                {
+                    /*from self to construct a shared_ptr obj
+                     * for destruct old obj
+                     *
+                     * we must know I won't change the value of other
+                     * I only to change the value of this*/
+                    this_type(other).swap(*this);
+                    return *this;
+                }
+                shared_ptr(const weak_ptr<T> &);
+                //shared_ptr();
+                /*because int the shared_ptr, we haven,t do new or malloc, 
+                 * so I need't to write destructor*/
+                //~shared_ptr();
         };
+};
+namespace hgg
+{
+    class weak_count
+    {
+        private:
+            friend class shared_count;
+            count_base *_pi;
+        public:
+            weak_count(const shared_count &sc) : _pi(sc._pi)
+            {
+                if (_pi != 0) _pi->add_weak_ref();
+            }
+            ~weak_count(){}
+    };
+    template <typename T>
+    class weak_ptr
+    {
+        private:
+            template <typename Type> friend class shared_ptr;
+            weak_count _pn;
+            T *_px;
+            weak_ptr(T *);
+            /*refuse the pointer copy*/
+        public:
+            shared_ptr<T> lock() const
+            {
+                return shared_ptr<T>(*this);
+            }
+            weak_ptr(const shared_ptr<T> &sp) 
+                : _px(sp._px), _pn(sp._pn){}
+            weak_ptr(const weak_ptr &wp)
+                : _px(wp.lock().get()), _pn(wp._pn){}
+            //~weak_ptr();
+    };
+    template <typename T>
+    shared_ptr<T>::shared_ptr(const weak_ptr<T> &wp) 
+        : _px(wp._px), _pn(wp._pn){}
+    shared_count::shared_count(const weak_count &wc) : _pi(wc._pi){
+        if (_pi != 0) _pi->add_use_count();
+    }
 };
 //int main()
 //{
